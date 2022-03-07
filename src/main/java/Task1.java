659417dc297f94927997357b34647855b18efd84
Galwik <gawlickikamil@gmail.com>
2022-03-07 14:43:54 +0100

    added Task1 and Task2

---
+++ src/main/java/Task1.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numberToGuess = new Random().nextInt(100);
+
+        System.out.println("Try to guess a number from 0 to 99:");
+        for (int i = 4; i >= 0; i--) {
+
+            int number = sc.nextInt();
+            if (number == numberToGuess) {
+                System.out.println("You guessed it!");
+                break;
+            } else if (number < numberToGuess && i != 0) {
+
+                System.out.println("Your number is LOWER than the one you are trying to guess");
+                System.out.println("You have " + i + " moves left");
+                System.out.println("Please try again");
+            } else if (number > numberToGuess && i != 0) {
+
+                System.out.println("Your number is GREATER than the one you are trying to guess");
+                System.out.println("You have " + i + " moves left");
+                System.out.println("Please try again");
+            }
+            if (i == 0) {
+
+                System.out.println("Sorry you didn't guess the number, the answer was: " + numberToGuess);
+            }
+        }
+
+
+    }
+
+
+}
---
+++ src/main/java/Task2.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter an integer:");
+        int size = scanner.nextInt();
+        int[] numbers = new int[size];
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 1; i <= size; i++) {
+
+            System.out.println("Enter a number: [" + i +"/"+size+ "] ");
+            int input = scanner.nextInt();
+            numbers[i - 1] = input;
+
+            if (i == 1) max = input;
+            if (input > max) max = input;
+
+        }
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        System.out.println("average of the numbers: " + (double) sum / size);
+        System.out.println("maximum value: " + max);
+
+    }
+}
